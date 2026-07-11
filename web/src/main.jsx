b7c0f61587b9fcdb3162ff79620e3553e12ee967
alchemmist <anton.ingrish@gmail.com>
2026-07-11 19:47:03 +0300

    redesign: mono theme (light-first) with dark toggle and hidden scrollbars

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider, ToasterProvider, ToasterComponent, Toaster } from "@gravity-ui/uikit";
 import "@gravity-ui/uikit/styles/fonts.css";
@@ -9,13 +9,27 @@ import App from "./App.jsx";
 
 const toaster = new Toaster();
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <ThemeProvider theme="light">
+function Root() {
+  const [theme, setTheme] = useState(() => localStorage.getItem("theme") || "light");
+  const toggleTheme = () =>
+    setTheme((t) => {
+      const next = t === "light" ? "dark" : "light";
+      localStorage.setItem("theme", next);
+      return next;
+    });
+
+  return (
+    <ThemeProvider theme={theme}>
       <ToasterProvider toaster={toaster}>
-        <App />
+        <App theme={theme} onToggleTheme={toggleTheme} />
         <ToasterComponent />
       </ToasterProvider>
     </ThemeProvider>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Root />
   </React.StrictMode>
 );
