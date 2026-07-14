a76e94c43b24071f38b5c8b385c01759a732cbf6
alchemmist <anton.ingrish@gmail.com>
2026-07-14 10:04:33 +0300

    Revert "docs: three concept landing variants (transaction, selectors, model) with switcher and ambient charts"
    This reverts commit 0da86e364fd29d34218131aba34684d0a544a45f.

--- web-docs/src/App.jsx
+++ web-docs/src/App.jsx
@@ -1,18 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Shell from "./components/Shell.jsx";
-import LandingV1 from "./components/LandingV1.jsx";
-import LandingV2 from "./components/LandingV2.jsx";
-import LandingV3 from "./components/LandingV3.jsx";
+import Landing from "./components/Landing.jsx";
 import MarkdownPage from "./components/MarkdownPage.jsx";
 
 export default function App({ theme, onToggleTheme }) {
   return (
     <Routes>
       <Route element={<Shell theme={theme} onToggleTheme={onToggleTheme} />}>
-        <Route index element={<LandingV1 />} />
-        <Route path="v1" element={<LandingV1 />} />
-        <Route path="v2" element={<LandingV2 />} />
-        <Route path="v3" element={<LandingV3 />} />
+        <Route index element={<Landing />} />
         <Route path=":slug" element={<MarkdownPage />} />
       </Route>
     </Routes>
