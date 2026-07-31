cd07f9d6771fa808fb8ca959a37d2408a0a8e099
alchemmist <anton.ingrish@gmail.com>
2026-07-31 23:11:01 +0300

    fix(migrations): type account card tails migration

--- server/migrations/versions/0013_account_card_tails.py
+++ server/migrations/versions/0013_account_card_tails.py
@@ -14,9 +14,9 @@ branch_labels = None
 depends_on = None
 
 
-def upgrade():
+def upgrade() -> None:
     op.execute("ALTER TABLE accounts ADD COLUMN card_tails TEXT NOT NULL DEFAULT ''")
 
 
-def downgrade():
+def downgrade() -> None:
     raise NotImplementedError("forward-only migrations")
